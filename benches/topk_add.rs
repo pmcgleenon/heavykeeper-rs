5e7580697efb54f82410e8f6b8203c86ae7cbc87
Patrick McGleenon <pmcgleenon@users.noreply.github.com>
2024-06-01 08:14:15 +0100

    Tidied up Add benchmark

--- benches/topk_add.rs
+++ benches/topk_add.rs
@@ -1,6 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand_distr::Distribution;
-use std::collections::HashMap;
 use zipf::ZipfDistribution;
 
 use heavykeeper::TopK;
@@ -8,22 +7,19 @@ use heavykeeper::TopK;
 fn benchmark_topk_add(c: &mut Criterion) {
 
     let mut rng = rand::thread_rng();
-    let zipf = ZipfDistribution::new(1000, 1.03).unwrap();
+    let zipf = ZipfDistribution::new(100_000, 1.03).unwrap();
     let mut topk = TopK::new(10, 1024, 5, 0.95);
 
-    let mut data_map = HashMap::new();
     let mut data = vec![];
-    for _ in 0..10000 {
+    for _ in 0..1_000_000 {
         let key = zipf.sample(&mut rng);
         data.push(key);
-        *data_map.entry(key).or_insert(0) += 1;
-        topk.add(key);
     }
 
     let mut group = c.benchmark_group("TopK_Add");
     group.sample_size(60); // Reduce the sample count
     group.warm_up_time(std::time::Duration::from_secs(3)); // Increase the warm-up time
-    group.measurement_time(std::time::Duration::from_secs(6)); // Increase the measurement time
+    group.measurement_time(std::time::Duration::from_secs(10)); // Increase the measurement time
 
     group.bench_function("Add", |b| {
         b.iter(|| {
