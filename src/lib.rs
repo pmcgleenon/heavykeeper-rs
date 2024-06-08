43a4e243d6ad624cd8a84809596e2a8c8f9f8603
Patrick McGleenon <pmcgleenon@users.noreply.github.com>
2024-06-08 10:36:40 +0100

    Added APACHE-2.0 license, some packaging updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! HeavyKeeper is a library for finding the Top N frequently occuring objects in a stream of data
+//!
+//! Top-K Heavykeeper algorithm for Top-K elephant flows
+//!
+//! This implementation is based on the paper HeavyKeeper: An Accurate Algorithm for Finding Top-k Elephant Flows
+//! by Junzhi Gong, Tong Yang, Haowei Zhang, and Hao Li, Peking University; Steve Uhlig, Queen Mary, University of London;
+//! Shigang Chen, University of Florida; Lorna Uden, Staffordshire University; Xiaoming Li, Peking University
+
+
 mod heavykeeper;
 
 pub use self::heavykeeper::*;
